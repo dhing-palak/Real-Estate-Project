506fe20a33046cc4ac96b6ff769c9951c9eafb50
Ayush Pegwar <ayushpegwar29081998@gmail.com>
2023-01-13 09:18:22 +0530

    Added common cities

--- root/src/common/common.js
+++ root/src/common/common.js
@@ -1,5 +1,21 @@
 export const city = [
   "Mumbai",
+  "Delhi",
+  "Kolkata",
+  "Hyderabad",
+  "Pune",
+  "Chennai",
+  "Patna",
+  "Bangalore",
+  "Lucknow",
+  "Noida",
+  "Kerala",
+];
+
+export const cities = [
+  "All",
+  "Mumbai",
+  "Delhi",
   "Kolkata",
   "Hyderabad",
   "Pune",
