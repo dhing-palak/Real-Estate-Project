0e8bcf8a86df3a390c1b5ca8ef923744016b46bb
Bisht <priyanka.c.bisht@accenture.com>
2023-02-10 11:59:12 +0530

    modified css

--- root/src/components/home/Home.js
+++ root/src/components/home/Home.js
@@ -32,7 +32,7 @@ const Home = () => {
 
   return (
     <>
-      <h5 className="username">Hello {userData.name} welcome to Real Estate.</h5>
+      <h5 className="username">Hello {userData.name} Welcome to Real Estate.</h5>
       <SearchProperty />
 
       <Property />
